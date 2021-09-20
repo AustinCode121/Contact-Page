a5b56147be551670651e70ef5eafc8255862a0a8
AustinCode121 <73318569+AustinCode121@users.noreply.github.com>
2021-09-20 10:25:07 -0400

    Update Upload.php

--- Upload.php
+++ Upload.php
@@ -2,7 +2,7 @@
 if (isset($_POST['Email'])) {
 
     // EDIT THE 2 LINES BELOW AS REQUIRED
-    $email_to = "mentalhealthbucket@gmail.com";
+    $email_to = "pokhrelau@sbschools.net";
     $email_subject = "contacted";
 
     function problem($error)
